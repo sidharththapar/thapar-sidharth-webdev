1379cde273785af6f13b21bcb53915cb53ba7a6d
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-05-12 05:59:36 -0400

    assignment1.1

--- test/app.js
+++ test/app.js
@@ -6,10 +6,10 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
     if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        var username = process.env.MLAB_WEBDEV_NEU_UNAME; // get from environment
+        var password = process.env.MLAB_WEBDEV_NEU_PASS;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds137261.mlab.com:37261/heroku_f875pvcm'; // user yours
     }
 
     var mongoose = require("mongoose");
