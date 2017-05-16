b24adf2f15c41c330f23a51d92bf77caba1f0533
sidharththapar <sidharththapar@yahoo.co.in>
2017-05-15 20:42:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
+app.get('/env', function(req, res) {
+  res.json(process.env);
+});
+
+//require ("./test/app.js")(app);
 
 var port = process.env.PORT || 3000;
 
