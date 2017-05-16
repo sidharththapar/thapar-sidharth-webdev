b0f22868699f1818ffd2cb83b7fc3d84d36daadb
sidharththapar <sidharththapar@yahoo.co.in>
2017-05-15 20:40:53 -0400

    Update app.js

--- test/app.js
+++ test/app.js
@@ -5,12 +5,16 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    
+    console.log('======>');
+    console.log(process.env.MLAB_WEBDEV_NEU_UNAME);
     if(process.env.MLAB_WEBDEV_NEU_UNAME) { // check if running remotely
         var username = process.env.MLAB_WEBDEV_NEU_UNAME; // get from environment
         var password = process.env.MLAB_WEBDEV_NEU_PASS;
         connectionString = 'mongodb://' + username + ':' + password;
         connectionString += '@ds139761.mlab.com:39761/heroku_v7g71n9h'; // user yours
     }
+    console.log('<======');
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
