1ae7f09ba04556b0d294d18cfe9bff89557d5df3
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-05-29 06:32:55 -0400

    few changes

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -1,3 +1,57 @@
 /**
  * Created by sidharththapar on 5/29/17.
  */
+(function () {
+    angular
+        .module('WAM')
+        .service('widgetService', widgetService);
+
+    function widgetService() {
+        this.findAllWidgetsForPage = findAllWidgetsForPage;
+        this.findWidgetById = findWidgetById;
+        this.deleteWidget = deleteWidget;
+        this.createWidget = createWidget;
+
+        var widgets = [
+            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                "url": "http://lorempixel.com/400/200/"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        ];
+
+        function createWidget(widget) {
+            widget._id = (new Date()).getTime() + "";
+            widgets.push(widget);
+        }
+
+        function deleteWidget(widgetId) {
+            var widget = findWidgetById(widgetId);
+            var index = widgets.indexOf(widget);
+            pages.splice(index, 1);
+        }
+
+        function findWidgetById(widgetId) {
+            return widgets.find(function (widget) {
+                return widget._id === widgetId;
+            });
+        }
+
+        function findAllWidgetsForPage(pageId) {
+            var results = [];
+
+            for(var v in widgets) {
+                if(widgets[v].pageId === pageId) {
+                    widgets[v].created = new Date();
+                    widgets[v].accessed = new Date();
+                    results.push(widgets[v]);
+                }
+            }
+            return results;
+        }
+    }
+})();
