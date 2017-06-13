71bba4217c5151cc6ca90a4b155d98a23f466ff2
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-06-13 00:05:37 -0400

    assignment5

--- public/assignment/directives/wam-directives.js
+++ public/assignment/directives/wam-directives.js
@@ -3,19 +3,21 @@
         .module('wamDirectives',[])
         .directive('wamSortable', wamSortable);
 
-    function wamSortable() {
+    function wamSortable($http) {
         return{
             link: linkFunction
         }
     }
     function linkFunction(scope, element) {
-        $(element).sortable({
-            start: function(event, ui){
-                var start = ui.item.index()},
+        $(element)
+            .sortable({
+                start: function(event, ui){
+                    var start = ui.item.index()},
 
-            stop: function(event, ui) {
-                var end = ui.item.index();
-            }});
+                stop: function(event, ui) {
+                    $http.put(ui.item.index());
+                    }})
+            .then()
     }
 
 
