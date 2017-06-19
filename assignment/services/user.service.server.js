0cbe90cf69f30c26aa76308e4f8ef81d02044fa0
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-06-18 20:02:40 -0400

    assignment 6.4

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -36,8 +36,8 @@ app.get('/auth/google',
 
 app.get('/auth/google/callback',
     passport.authenticate('google', {
-        successRedirect: 'assignment/index.html#!/profile',
-        failureRedirect: 'assignment/index.html#!/login'
+        successRedirect: '/profile',
+        failureRedirect: '/login'
     }));
 
 function isAdmin(req, res, next) {
