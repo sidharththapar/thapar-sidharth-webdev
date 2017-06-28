b07f4f11b5090b846a368c0545c0203444307047
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-06-28 11:35:41 -0400

    heroku error

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -17,7 +17,7 @@ var googleConfig = {
     clientID     : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
     callbackURL  : process.env.GOOGLE_CALLBACK_URL
-// };
+};
 passport.use(new GoogleStrategy(googleConfig, googleStrategy));
 
 
